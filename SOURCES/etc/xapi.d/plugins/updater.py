52be98624ebbd4e081511ac12b6711a79f573bde
Nicolas Raynaud <github@nraynaud.com>
2018-05-23 13:19:31 -0700

    added rpm creation and a quick test script

--- SOURCES/etc/xapi.d/plugins/updater.py
+++ SOURCES/etc/xapi.d/plugins/updater.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import subprocess
 import sys
 import traceback
 import XenAPIPlugin
@@ -16,14 +17,37 @@ LOG_FILE = "/var/log/updater.log"
 ENABLE_DEV_LOGGING_FILE = "/opt/xensource/packages/files/updater/devlogging_enabled"
 
 
+def display_package(p):
+    return {'name': p.name, 'version': p.version, 'release': p.release, 'arch': p.arch, 'fullName': p.ui_envra}
+
+
+def run_command(command):
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    result = {'exit': process.returncode, 'stdout': stdout, 'stderr': stderr, 'command': command}
+    _LOGGER.info(result)
+    return result
+
+
 def check_update(session, args):
     yum_instance = yum.YumBase()
     yum_instance.preconf.debuglevel = 0
     yum_instance.preconf.plugins = True
     packages = yum_instance.doPackageLists(pkgnarrow='updates')
-    return json.dumps({'result': map(
-        lambda p: {'name': p.name, 'version': p.version, 'release': p.release, 'arch': p.arch, 'fullName': p.ui_envra},
-        packages)})
+    del yum_instance.ts
+    yum_instance.initActionTs()  # make a new, blank ts to populate
+    yum_instance.populateTs(keepold=0)
+    yum_instance.ts.check()  # required for ordering
+    yum_instance.ts.order()
+    return json.dumps({'result': map(display_package, packages)})
+
+
+def update(session, args):
+    packages = args.get('packages')
+    command = ['yum', 'update', '-y']
+    if packages:
+        command.append(packages)
+    return json.dumps(run_command(command))
 
 
 def handle_unhandled_exceptions(exception_type, exception_value,
@@ -70,10 +94,11 @@ def configure_logging():
     signal.signal(signal.SIGPIPE, signal.SIG_DFL)
 
 
-_LOGGER = logging.getLogger()
+_LOGGER = logging.getLogger('yum')
 configure_logging()
 sys.excepthook = handle_unhandled_exceptions
 if __name__ == "__main__":
     XenAPIPlugin.dispatch({
-        'check_update': check_update
+        'check_update': check_update,
+        'update': update
     })
--- test_plugin.sh
+++ test_plugin.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 
-set -e
+set -ex
 
-TEST_HOST=root@192.168.100.187
+MASTER_HOST=root@192.168.100.11
+XCP_HOST_UNDER_TEST=root@192.168.100.187
+SNAPSHOT_UUID=581cdbe3-5fc1-695c-d2ba-c73cbf4ea24f
+VM_HOST_UNDER_TEST_UUID=58b2d434-b3d3-d607-dde9-7a0f69201c08
+HOST_UNDER_TEST_UUID=83e9bfc6-cfc3-4873-9f6b-b8df6d89e87a
 
-rsync -v  --exclude '.*' -r xcp-ng-updater/ ${TEST_HOST}:/
-ssh ${TEST_HOST} xe host-call-plugin host-uuid=83e9bfc6-cfc3-4873-9f6b-b8df6d89e87a plugin=updater.py fn=check_update
+ssh ${MASTER_HOST} xe snapshot-revert snapshot-uuid=${SNAPSHOT_UUID}
+ssh ${MASTER_HOST} xe vm-start vm=${VM_HOST_UNDER_TEST_UUID}
+until ping -c1 192.168.100.187 &>/dev/null; do :; done
+sleep 20
+rsync -v  --exclude '.*' -r xcp-ng-updater_1.0.rpm ${XCP_HOST_UNDER_TEST}:
+ssh ${XCP_HOST_UNDER_TEST} rpm -i xcp-ng-updater_1.0.rpm
+sleep 10
+ssh ${XCP_HOST_UNDER_TEST} xe host-call-plugin host-uuid=${HOST_UNDER_TEST_UUID} plugin=updater.py fn=check_update
+ssh ${XCP_HOST_UNDER_TEST} xe host-call-plugin host-uuid=${HOST_UNDER_TEST_UUID} plugin=updater.py fn=update
