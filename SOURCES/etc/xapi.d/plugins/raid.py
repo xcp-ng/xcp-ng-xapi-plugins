c57c3e5f1770ad9ff054e619be6bd5c7e13a69a5
Nicolas Raynaud <github@nraynaud.com>
2019-07-17 17:28:06 -0700

    add raid information retrieval

---
+++ SOURCES/etc/xapi.d/plugins/raid.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import json
+
+import XenAPIPlugin
+
+sys.path.append('.')
+from xcpngutils import configure_logging, run_command
+
+_LOGGER = configure_logging('raid')
+
+
+# returns a json {"status": {"State": "clean", (...)}, "volumes": [["0", "8", "0", "0", "active sync", "/dev/sda"], (...)]}
+# @pidfile(timeout=10)
+def check_raid_pool(session, args):
+    device = '/dev/md127'
+    result = run_command(['mdadm', '--detail', device])
+
+    lines = [line.strip() for line in result['stdout'].splitlines()]
+    lines = [line for line in lines if len(line) > 0]
+    # remove first line ('/dev/md127:')
+    lines = lines[1:]
+    # look for the line 'Number   Major   Minor   RaidDevice State'
+    footer_index = next(i for i, line in enumerate(lines) if line.startswith('Number'))
+    # '1       8       16        1      active sync   /dev/sdb' -> ["1", "8", "16", "1", "active sync", "/dev/sdb"]
+    volumes = [[field.strip() for field in line.split('  ') if len(field.strip()) > 0] for line in
+               lines[footer_index + 1:]]
+    # 'Version : 1.0' -> {"Version ": " 1.0"}
+    lines = dict([[element.strip() for element in line.split(' : ', 1)] for line in lines[0:footer_index]])
+    return json.dumps({'status': lines, 'volumes': volumes})
+
+
+if __name__ == "__main__":
+    XenAPIPlugin.dispatch({
+        'check_raid_pool': check_raid_pool
+    })
