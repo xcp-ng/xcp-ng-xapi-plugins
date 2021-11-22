47b85f08fc8aee4aa539be5675c997584c3cb6f9
Ronan Abhamon <ronan.abhamon@vates.fr>
2021-11-22 14:56:04 +0100

    ConfigParser mock is now configurable
    Signed-off-by: Ronan Abhamon <ronan.abhamon@vates.fr>

--- tests/mocked_configparser.py
+++ tests/mocked_configparser.py
@@ -1,55 +1,34 @@
-class ConfigParser:
-    def __init__(self, dict=None):
-        return
+class ConfigParser(object):
+    def __init__(self, defaults={}):
+        self._defaults = defaults
+        self._sections = {}
 
     def sections(self):
-        return ["repo_yum", "repo_yum_2", "repo_yum_3"]
+        raise Exception('Mock me!')
+
+    def read(self, filepath):
+        raise Exception('Mock me!')
+
+    def write(self, filepath):
+        raise Exception('Mock me!')
 
     def has_section(self, section_name):
         return section_name in self.sections()
 
     def has_option(self, section_name, field_name):
         try:
-            self.read_sections[section_name][field_name]
+            self._sections[section_name][field_name]
             return True
-        except Exception as e:
+        except Exception:
             return False
 
     def get(self, section_name, field_name, default_value=None):
         try:
-            section = self.read_sections[section_name]
+            section = self._sections[section_name]
             field = section[field_name]
             return field
-        except Exception as e:
+        except Exception:
             return default_value
 
     def set(self, section_name, field_name, value):
-        self.read_sections[section_name][field_name] = value
-
-    def read(self, filepath):
-        self.read_sections = {
-            "repo_yum": {
-                "name": "yum_repo",
-                "baseurl": "http://yumrepo.example.com/os",
-                "enabled": "1",
-                "gpgcheck": "0",
-                "proxy": "http://user:password@proxy.example.com:3128"
-            },
-            "repo_yum_2": {
-                "name": "yum_repo_2",
-                "baseurl": "http://yumrepo.example.com/os",
-                "enabled": "1",
-                "gpgcheck": "0",
-                "proxy": "_none_"
-            },
-            "repo_yum_3": {
-                "name": "yum_repo_3",
-                "baseurl": "http://yumrepo.example.com/os",
-                "enabled": "1",
-                "gpgcheck": "0"
-            }
-        }
-        return [filepath]
-
-    def write(self, file):
-        return 0
+        self._sections[section_name][field_name] = value
--- tests/test_updater.py
+++ tests/test_updater.py
@@ -1,9 +1,14 @@
+from ConfigParser import ConfigParser
 import json
 import mock
 import pytest
 import XenAPIPlugin
 
 from updater import check_update, get_proxies, set_proxies, update, DEFAULT_REPOS
+
+# ==============================================================================
+# Update.
+# ==============================================================================
 
 class TestCheckUpdate:
     def test_check_update(self, fs):
@@ -73,42 +78,79 @@ class TestUpdate:
             ['yum', 'update', '--disablerepo="*"', '--enablerepo=' + ','.join(repos), '-y', packages]
         )
 
+# ==============================================================================
+# Proxies.
+# ==============================================================================
+
+class MockedUpdaterConfigParser(ConfigParser):
+    def __init__(self, defaults={}):
+        self._defaults = defaults
+        self._sections = {}
+
+    def sections(self):
+        return ['repo_yum', 'repo_yum_2', 'repo_yum_3']
+
+    def read(self, filepath):
+        self._sections = {
+            'repo_yum': {
+                'name': 'yum_repo',
+                'baseurl': 'http://yumrepo.example.com/os',
+                'enabled': '1',
+                'gpgcheck': '0',
+                'proxy': 'http://user:password@proxy.example.com:3128'
+            },
+            'repo_yum_2': {
+                'name': 'yum_repo_2',
+                'baseurl': 'http://yumrepo.example.com/os',
+                'enabled': '1',
+                'gpgcheck': '0',
+                'proxy': '_none_'
+            },
+            'repo_yum_3': {
+                'name': 'yum_repo_3',
+                'baseurl': 'http://yumrepo.example.com/os',
+                'enabled': '1',
+                'gpgcheck': '0'
+            }
+        }
+        return [filepath]
+
+    def write(self, filepath):
+        return 0
+
+CONFIG_PROXIES = '{"repo_yum": "http://user:password@proxy.example.com:3128", "repo_yum_2": "_none_", "repo_yum_3": "_none_"}' # noqa: E501
+
+@mock.patch('ConfigParser.ConfigParser', new_callable=lambda: MockedUpdaterConfigParser)
 class TestGetProxies:
-    def test_get_proxies(self, fs):
-        expected = ' \
-            {"repo_yum": "http://user:password@proxy.example.com:3128", "repo_yum_2": "_none_", "repo_yum_3": "_none_"}'
+    def test_get_proxies(self, ConfigParser, fs):
         res = get_proxies(None, None)
-        assert json.loads(expected) == json.loads(res)
+        assert json.loads(CONFIG_PROXIES) == json.loads(res)
 
-    @mock.patch('mocked_configparser.ConfigParser.read', autospec=True)
-    def test_get_proxies_error(self, read, fs):
-        read.side_effect = [Exception("Error!")]
+    def test_get_proxies_error(self, ConfigParser, fs):
+        with mock.patch.object(ConfigParser, 'read') as read:
+            read.side_effect = [Exception("Error!")]
 
-        with pytest.raises(XenAPIPlugin.Failure) as e:
-            get_proxies(None, None)
-        read.assert_called_once()
-        assert e.value.params[0] == '-1'
-        assert e.value.params[1] == 'Error!'
+            with pytest.raises(XenAPIPlugin.Failure) as e:
+                get_proxies(None, None)
+            read.assert_called_once()
+            assert e.value.params[0] == '-1'
+            assert e.value.params[1] == 'Error!'
 
+@mock.patch('ConfigParser.ConfigParser', new_callable=lambda: MockedUpdaterConfigParser)
 class TestSetProxies:
-    def test_set_proxies(self, fs):
+    def test_set_proxies(self, ConfigParser, fs):
         fs.create_dir('/etc/yum.repos.d')
 
-        proxies = ' \
-            {"repo_yum": "http://user:password@proxy.example.com:3128", "repo_yum_2": "_none_", "repo_yum_3": "_none_"}'
-        res = set_proxies(None, {"proxies": proxies})
+        res = set_proxies(None, {"proxies": CONFIG_PROXIES})
         assert res == ""
 
-    @mock.patch('mocked_configparser.ConfigParser.write', autospec=True)
-    def test_set_proxies_error(self, write, fs):
+    def test_set_proxies_error(self, ConfigParser, fs):
         fs.create_dir('/etc/yum.repos.d')
 
-        write.side_effect = [Exception('Error!')]
-
-        proxies = ' \
-            {"repo_yum": "http://user:password@proxy.example.com:3128", "repo_yum_2": "_none_", "repo_yum_3": "_none_"}'
-        with pytest.raises(XenAPIPlugin.Failure) as e:
-            set_proxies(None, {"proxies": proxies})
-        write.assert_called_once()
-        assert e.value.params[0] == '-1'
-        assert e.value.params[1] == 'Error!'
+        with mock.patch.object(ConfigParser, 'write') as write:
+            write.side_effect = [Exception('Error!')]
+            with pytest.raises(XenAPIPlugin.Failure) as e:
+                set_proxies(None, {"proxies": CONFIG_PROXIES})
+            write.assert_called_once()
+            assert e.value.params[0] == '-1'
+            assert e.value.params[1] == 'Error!'
