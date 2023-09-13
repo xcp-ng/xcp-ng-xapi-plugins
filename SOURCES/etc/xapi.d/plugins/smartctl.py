37b3b2608c93869cb58b42a8032520895cd7db4f
Cécile MORANGE <contact@ataxya.net>
2023-09-13 15:58:02 +0200

    push suggested changes + add unit testing
    Signed-off-by: Cécile MORANGE <contact@ataxya.net>

--- SOURCES/etc/xapi.d/plugins/smartctl.py
+++ SOURCES/etc/xapi.d/plugins/smartctl.py
@@ -3,62 +3,49 @@
 
 import json
 import sys
-import subprocess
 import XenAPIPlugin
 
 sys.path.append('.')
 from xcpngutils import configure_logging, run_command, error_wrapped
 from xcpngutils.operationlocker import OperationLocker
 
-_LOGGER = configure_logging('smartctl')
-
-def list_disks():
+@error_wrapped
+def _list_disks():
     disks = []
-    try:
-        output = subprocess.check_output(['smartctl', '--scan'])
-        for line in output.splitlines():
-            if line.startswith('/dev/') and not line.startswith('/dev/bus/'):
-                disks.append(line.split()[0])
-    except subprocess.CalledProcessError as e:
-        results[disks] = "Error: " + str(e)
+    result = run_command(['smartctl', '--scan'])
+    for line in result['stdout'].splitlines():
+        if line.startswith('/dev/') and not line.startswith('/dev/bus/'):
+            disks.append(line.split()[0])
     return disks
 
 @error_wrapped
-def check_smartctl(a,b):
+def get_information(session, args):
+    results = {}
     with OperationLocker():
-        results = {}
-        disks = list_disks()
+        disks = _list_disks()
         for disk in disks:
-            cmd = ["smartctl", "-j", "-a", disk]
-            try:
-                output = subprocess.check_output(cmd)
-                json_output = json.loads(output)
-                results[disk] = json.loads(output)
-            except subprocess.CalledProcessError as e:
-                results[disk] = "Error: " + str(e)
+            cmd = run_command(["smartctl", "-j", "-a", disk])
+            results[disk] = json.loads(cmd['stdout'])
         return json.dumps(results)
 
 @error_wrapped
-def check_health(a,b):
+def get_health(session, args):
+    results = {}
     with OperationLocker():
-        results = {}
-        disks = list_disks()
+        disks = _list_disks()
         for disk in disks:
-            cmd = ["smartctl", "-j", "-H", disk]
-            try:
-                output = subprocess.check_output(cmd)
-                json_output = json.loads(output)
-                if json_output['smart_status']['passed']:
-                    results[disk] = "PASSED"
-                else:
-                    results[disk] = "FAILED"
-            except subprocess.CalledProcessError as e:
-                results[disk] = "Error: " + str(e)
+            cmd = run_command(["smartctl", "-j", "-H", disk])
+            json_output = json.loads(cmd['stdout'])
+            if json_output['smart_status']['passed']:
+                results[disk] = "PASSED"
+            else:
+                results[disk] = "FAILED"
         return json.dumps(results)
 
 
+_LOGGER = configure_logging('smartctl')
 if __name__ == "__main__":
     XenAPIPlugin.dispatch({
-        'information': check_smartctl,
-        'health': check_health
+        'information': get_information,
+        'health': get_health
     })
