1af86a5877e36b53b35740e4b4cff5fdb22388ef
1234Erwan <84467245+1234Erwan@users.noreply.github.com>
2024-08-22 22:39:28 +0200

    Update smartctl.py
    Try to add hard raid support for smartctl.py
    
    Signed-off-by: 1234Erwan <84467245+1234Erwan@users.noreply.github.com>

--- SOURCES/etc/xapi.d/plugins/smartctl.py
+++ SOURCES/etc/xapi.d/plugins/smartctl.py
@@ -14,32 +14,47 @@ def _list_disks():
     disks = []
     result = run_command(['smartctl', '--scan'])
     for line in result['stdout'].splitlines():
-        if line.startswith('/dev/') and not line.startswith('/dev/bus/'):
-            disks.append(line.split()[0])
+        disks.append(line.split()[0])
     return disks
+
+@error_wrapped
+def _list_raids():
+    raids = []
+    result = run_command(['smartctl', '--scan'])
+    for line in result['stdout'].splitlines():
+        disks.append(line.split()[2])
+    return raids
 
 @error_wrapped
 def get_information(session, args):
     results = {}
+    raids = {}
+    i = 0
     with OperationLocker():
         disks = _list_disks()
+        raids = _list_raids()
         for disk in disks:
-            cmd = run_command(["smartctl", "-j", "-a", disk], check=False)
+            cmd = run_command(["smartctl", "-j", "-a", "-d", raids[i], disk], check=False)
             results[disk] = json.loads(cmd['stdout'])
+            i = i + 1
         return json.dumps(results)
 
 @error_wrapped
 def get_health(session, args):
     results = {}
+    raids = {}
+    i = 0
     with OperationLocker():
         disks = _list_disks()
+        raids = _list_raids()
         for disk in disks:
-            cmd = run_command(["smartctl", "-j", "-H", disk])
+            cmd = run_command(["smartctl", "-j", "-H", "-d", raids[i], disk])
             json_output = json.loads(cmd['stdout'])
             if json_output['smart_status']['passed']:
                 results[disk] = "PASSED"
             else:
                 results[disk] = "FAILED"
+            i = i + 1
         return json.dumps(results)
 
 
