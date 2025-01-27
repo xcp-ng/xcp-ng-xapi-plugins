45ab56aa3bac0ec1adfa262499fdeeb8e1bd5237
Guillaume <guillaume.thouvenin@vates.tech>
2025-01-27 17:27:43 +0100

    Fixes inversion of parameters passed to smartctl
    The `-d` parameter expects the type of the device and not its name. This
    patch fixes the issue.
    
    Signed-off-by: Guillaume <guillaume.thouvenin@vates.tech>

--- SOURCES/etc/xapi.d/plugins/smartctl.py
+++ SOURCES/etc/xapi.d/plugins/smartctl.py
@@ -14,7 +14,7 @@ def _list_devices():
     devices = []
     result = run_command(['smartctl', '--scan'])
     for line in result['stdout'].splitlines():
-        devices.append({'name': line.split()[0], 'type':line.split()[2]})
+        devices.append({'name': line.split()[0], 'type': line.split()[2]})
     return devices
 
 @error_wrapped
@@ -23,7 +23,7 @@ def get_information(session, args):
     with OperationLocker():
         devices = _list_devices()
         for device in devices:
-            cmd = run_command(["smartctl", "-j", "-a", "-d", device['name'], device['type']], check=False)
+            cmd = run_command(["smartctl", "-j", "-a", "-d", device['type'], device['name']], check=False)
             results[device] = json.loads(cmd['stdout'])
         return json.dumps(results)
 
@@ -33,7 +33,7 @@ def get_health(session, args):
     with OperationLocker():
         devices = _list_devices()
         for device in devices:
-            cmd = run_command(["smartctl", "-j", "-H", "-d", device['name'], device['type']], check=False)
+            cmd = run_command(["smartctl", "-j", "-H", "-d", device['type'], device['name']], check=False)
             json_output = json.loads(cmd['stdout'])
             if json_output['smart_status']['passed']:
                 results[disk] = "PASSED"
